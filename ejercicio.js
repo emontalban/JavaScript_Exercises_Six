32f933ffb387d8545c523b00cc5efd578bcc846e
emontalban <shamanessemmp@gmail.com>
2026-03-31 23:07:21 +0200

    añadido el ejercicio

---
+++ ejercicio.js
@@ -0,0 +1,13 @@
+function Suma (num1, num2, num3, num4){
+    let resultado = (num1 + num2) * (num3 + num4);
+    if(resultado > 50)
+    {
+        console.log ("¡El número es mayor que 50!");
+    } else
+    {
+         console.log("¡El número es menor que 50!");
+    } 
+}
+
+Suma(3,4,7,8);
+Suma(1,2,3,4);
